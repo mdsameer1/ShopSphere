c4ff8eda09d5ca3f53543b27373a0871db7e7dac
samm043 <samm043@users.noreply.github.com>
2026-03-06 08:42:07 +0530

    completed user registration logic

---
+++ backend/controllers/userController.js
@@ -0,0 +1,40 @@
+import { User } from "../models/userModel.js";
+import bcrypt from "bcryptjs"
+
+export const register = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    if (!firstName || !lastName || !email || !password) {
+      res.status(400).json({
+        success: false,
+        message: "all fields are required",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      res.status(400).json({
+        success: false,
+        message: "user alredy exist",
+      });
+    }
+
+    const hashPassword=await bcrypt.hash(password,10)
+    const newUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      password:hashPassword
+    });
+    await newUser.save();
+    return res.status(201).json({
+      success: true,
+      message: "user registered successfully",
+      user: newUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
