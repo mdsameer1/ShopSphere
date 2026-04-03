ef8428e8c4887dc1cddc87474324f9271a9a3d6f
samm043 <samm043@users.noreply.github.com>
2026-04-03 16:27:49 +0530

    developed signup page with the help of react,shadcn and tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,42 @@
-export default function App() {
+import React from 'react'
+import { Button } from './components/ui/button'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import {Home} from './pages/home'
+import {Navbar} from './components/navbar'
+import {Signup} from './pages/signup'
+import {Login} from './pages/login'
+
+ const router=createBrowserRouter( [
+  {
+    path:'/',
+    element:<>
+    <Navbar></Navbar>
+    <Home></Home>
+
+    </>
+  },
+  {
+    path:'/signup',
+    element:<>
+    <Signup></Signup>
+
+    </>
+  },
+  {
+    path:'/login',
+    element:<>
+    <Login></Login>
+
+    </>
+  }
+ ])
+
+function App() {
   return (
-    <h1 className="text-red-700">
-      Hello world!
-    </h1>
+    <>
+    <RouterProvider router={router}/>
+    </>
   )
 }
+
+export default App
---
+++ frontend/src/pages/signup.jsx
@@ -0,0 +1,133 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import axios from "axios";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+
+
+export function Signup() {
+  const [showPassword,setShowPassword]=useState(false)
+  const [loading,setLoading]=useState(false)
+  const [formData,setFormData]=useState({
+    firstName:"",
+    lastName:"",
+    email:"",
+    password:""
+  })
+  const navigate=useNavigate()
+  const handleChange=(e)=>{
+    const {name,value}=e.target;
+    setFormData((prev)=>({
+      ...prev,
+      [name]:value
+    }))
+  }
+  const submitHandler=async(e)=>{
+    e.preventDefault();
+    console.log(formData);
+    try {
+      const res=await axios.post(`http://localhost:3000/api/v1/user/register`,formData,{headers:{
+        'Content-Type':"application/json"
+      }})
+      if(res.data.success){
+         navigate("/verify")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  } 
+      return (
+    <div className="flex justify-center items-center min-h-screen bg-pink-100">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Create your account</CardTitle>
+          <CardDescription>
+            Enter your email and password below to Create to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          
+            <div className="flex flex-col gap-3">
+              <div className="grid grid-cols-2 gap-2">
+                <div className="grid gap-2">
+                  <Label htmlFor="firstName">First Name</Label>
+                  <Input
+                    id="firstName"
+                    name="firstName"
+                    type="text"
+                    placeholder="John"
+                    required
+                    value={formData.firstName}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="lastName">Last Name</Label>
+                  <Input
+                    id="lastName"
+                    name="lastName"
+                    type="text"
+                    placeholder="Doe"
+                    required
+                    value={formData.lastName}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="email@example.com"
+                  required
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <div className="relative">
+                  <Input
+                    id="password"
+                    name="password"
+                    placeholder="create a password"
+                    type={showPassword? 'text':'password'}
+                    required
+                    value={formData.password}
+                    onChange={handleChange}
+                  />
+                  {
+                  showPassword ? <EyeOff onClick={()=>setShowPassword(false)} className="w-5 h-5  text-gray-700 absolute right-5 bottom-2"/>:
+                  <Eye onClick={()=>setShowPassword(true)} className="w-5 h-5 text-gray-700 absolute right-5 bottom-2 "/>
+                  }
+                </div> 
+              </div>
+            </div>
+          
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button onClick={submitHandler} type="submit" className="w-full cursor-pointer hover:bg-pink-600">
+           Signup
+          </Button>
+          <p className="text-gray-700 text-sm">Already have an account?  <Link to={"/login"} className="hover:underline cursor-pointer text-pink-800">Login</Link></p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
